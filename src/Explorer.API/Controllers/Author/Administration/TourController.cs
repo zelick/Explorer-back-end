1193b1b312f9bcfa69d0da94772872b384fcf2af
Milica Vujić <milicavujic2001@gmail.com>
2023-10-15 13:36:11 +0200

    fix:Tour model fixed

---
+++ src/Explorer.API/Controllers/Author/Administration/TourController.cs
@@ -0,0 +1,42 @@
+﻿using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Explorer.Tours.Core.UseCases.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Author.Administration
+{
+    [Authorize(Policy = "authorPolicy")]
+    [Route("api/administration/tour")]
+    public class TourController : BaseApiController
+    {
+        private readonly ITourService _tourService;
+
+        public TourController(ITourService tourService)
+        {
+            _tourService = tourService;
+        }
+
+        [HttpPost]
+        public ActionResult<TourDto> Create([FromBody] TourDto tour)
+        {
+            var result = _tourService.Create(tour);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<TourDto> Update([FromBody] TourDto tour)
+        {
+            var result = _tourService.Update(tour);
+            return CreateResponse(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var result = _tourService.Delete(id);
+            return CreateResponse(result);
+        }
+
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TourDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class TourDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int? DemandignessLevel { get; set; }
+        public double Price { get; set; }
+        public List<string>? Tags { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
+
+
