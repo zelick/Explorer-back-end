97bc90b3e522c2d7c28aaf954a4d35030d6d4333
Matija Maksimović <116630740+MatijaMax@users.noreply.github.com>
2023-10-18 00:51:24 +0200

    feat: Created preference model.

---
+++ src/Modules/Tours/Explorer.Tours.API/Public/Administration/ITourPreferenceService.cs
@@ -0,0 +1,18 @@
+﻿using Explorer.Tours.API.Dtos;
+using FluentResults;
+
+namespace Explorer.Tours.API.Public.Administration
+{
+    public interface ITourPreferenceService
+    {
+
+        Result<TourPreferenceDto> GetPreferenceByCreator(int page, int pageSize, int id);
+        Result<TourPreferenceDto> GetPaged(int page, int pageSize);
+        Result<TourPreferenceDto> Create(TourPreferenceDto preference);
+        Result<TourPreferenceDto> Update(TourPreferenceDto preference);
+        Result Delete(int id);
+
+
+
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourPreference.cs
@@ -0,0 +1,35 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class TourPreference : Entity
+    {
+        public int Id { get; init; }
+        public int CreatorId { get; init; }
+        public TourDifficulty? Difficulty { get; init; }
+        public int Walk { get; init; } = 0;
+        public int Bike { get; init; } = 0;
+        public int Car { get; init; } = 0;
+        public int Boat { get; init; } = 0;
+        public List<string>? Tags { get; init; }
+
+        public TourPreference(int id, int creatorId, TourDifficulty? difficulty, int walk, int bike, int car, int boat, List<string>? tags)
+        {
+            if (id == 0) throw new ArgumentException("Invalid preference");
+            if (creatorId == 0) throw new ArgumentException("Inavlid creator");
+            CreatorId = creatorId;
+            Difficulty = difficulty;
+            Walk = walk;
+            Bike = bike;
+            Car = car;
+            Boat = boat;
+            Tags = tags;
+        }
+    }
+    public enum TourDifficulty
+    {
+        Easy = 0,
+        Medium = 1,
+        Hard = 2
+    }
+}
