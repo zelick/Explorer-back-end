5e5141a934bd241f5ddfcaae48413cb2d0a6c7af
somelijer <116906162+somelijer@users.noreply.github.com>
2023-10-18 22:15:02 +0200

    Added tests

--- src/Explorer.API/Controllers/Author/Administration/MapObjectController.cs
+++ src/Explorer.API/Controllers/Author/Administration/MapObjectController.cs
@@ -24,5 +24,26 @@ namespace Explorer.API.Controllers.Author.Administration
             var result = _mapObjectService.GetPaged(page, pageSize);
             return CreateResponse(result);
         }
+
+        [HttpPost]
+        public ActionResult<MapObjectDto> Create([FromBody] MapObjectDto mapObject)
+        {
+            var result = _mapObjectService.Create(mapObject);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<MapObjectDto> Update([FromBody] MapObjectDto mapObject)
+        {
+            var result = _mapObjectService.Update(mapObject);
+            return CreateResponse(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var result = _mapObjectService.Delete(id);
+            return CreateResponse(result);
+        }
     }
 }
