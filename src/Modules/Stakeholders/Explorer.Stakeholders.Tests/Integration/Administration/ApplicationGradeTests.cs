971f349b5d96f0cfc96167203a2eef5dda35c029
Stefaan01 <116906194+Stefaan01@users.noreply.github.com>
2023-10-19 02:36:17 +0200

    feat: first dynamic test of application grade

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/Integration/Administration/ApplicationGradeTests.cs
@@ -0,0 +1,47 @@
+﻿using Explorer.API.Controllers.Tourist.Tour;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Explorer.Stakeholders.Infrastructure.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.Tests.Integration.Administration
+{
+    public class ApplicationGradeTests : BaseStakeholdersIntegrationTest
+    {
+        public ApplicationGradeTests(StakeholdersTestFactory factory) : base(factory)
+        {
+
+        }
+
+
+        [Fact]
+        public void GiveHigherRating()
+        {
+            //Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var dto = new ApplicationGradeDto();
+            dto.Rating = 6;
+            dto.Comment = "This is the best app I have ever used";
+
+            //Act
+            var result = (ObjectResult)controller.EvaluateApplication(dto).Result;
+
+            //Assert
+            result.StatusCode.ShouldBe(500);
+        }
+
+        private static ApplicationGradeController CreateController(IServiceScope scope)
+        {
+            return new ApplicationGradeController(scope.ServiceProvider.GetRequiredService<IApplicationGradeService>());
+        }
+    }
+}
---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/d-grades.sql
@@ -0,0 +1,15 @@
+INSERT INTO stakeholders."ApplicationGrades"(
+	"Id", "Rating", "Comment")
+	VALUES (4, 1, 'Extremly bad');
+INSERT INTO stakeholders."ApplicationGrades"(
+	"Id", "Rating", "Comment")
+	VALUES (5, 2, 'Very bad');
+INSERT INTO stakeholders."ApplicationGrades"(
+	"Id", "Rating", "Comment")
+	VALUES (6, 3, 'Good, but it can be better');
+INSERT INTO stakeholders."ApplicationGrades"(
+	"Id", "Rating", "Comment")
+	VALUES (7, 4, 'Very, very good app');
+INSERT INTO stakeholders."ApplicationGrades"(
+	"Id", "Rating", "Comment")
+	VALUES (8, 5, 'Brilliant');
