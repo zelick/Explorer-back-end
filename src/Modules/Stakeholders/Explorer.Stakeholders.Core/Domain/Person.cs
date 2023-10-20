73bcbc03a958be9a57e5c3846e7eea2c0aaf3fee
draskonovakovic <116906185+draskonovakovic@users.noreply.github.com>
2023-10-20 09:08:02 +0200

    added validation for name and surname

--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/Person.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/Person.cs
@@ -1,5 +1,6 @@
 ﻿using Explorer.BuildingBlocks.Core.Domain;
 using System.Net.Mail;
+using System.Security.Principal;
 
 namespace Explorer.Stakeholders.Core.Domain;
 
@@ -30,6 +31,13 @@ public class Person : Entity
         if (UserId == 0) throw new ArgumentException("Invalid UserId");
         if (string.IsNullOrWhiteSpace(Name)) throw new ArgumentException("Invalid Name");
         if (string.IsNullOrWhiteSpace(Surname)) throw new ArgumentException("Invalid Surname");
+        if (!IsValidName(Name) || !IsValidName(Surname)) throw new ArgumentException("Name and Surname should start with upper letter");
         if (!MailAddress.TryCreate(Email, out _)) throw new ArgumentException("Invalid Email");
     }
+
+    private bool IsValidName(string name)
+    {
+        return !string.IsNullOrWhiteSpace(name) && char.IsUpper(name[0]);
+    }
+
 }
