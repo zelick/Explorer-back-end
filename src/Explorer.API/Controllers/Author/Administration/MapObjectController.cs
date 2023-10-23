b7a200573e8e23bea1893cd07de400c1c67ad0ff
somelijer <116906162+somelijer@users.noreply.github.com>
2023-10-23 17:55:45 +0200

    Revert "Revert "Merge pull request #15 from cetvrta-grupa/feat/map-objects""
    This reverts commit ccb7426b7eb1d63f065f55d3a724bd34cf14da2a.

---
+++ src/Explorer.API/Controllers/Author/Administration/MapObjectController.cs
@@ -0,0 +1,49 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Explorer.Tours.Core.UseCases.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Author.Administration
+{
+    [Authorize(Policy = "authorPolicy")]
+    [Route("api/administration/mapObject")]
+    public class MapObjectController : BaseApiController
+    {
+        private readonly IMapObjectService _mapObjectService;
+
+        public MapObjectController(IMapObjectService mapObjectService)
+        {
+            _mapObjectService = mapObjectService;
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<MapObjectDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _mapObjectService.GetPaged(page, pageSize);
+            return CreateResponse(result);
+        }
+
+        [HttpPost]
+        public ActionResult<MapObjectDto> Create([FromBody] MapObjectDto mapObject)
+        {
+            var result = _mapObjectService.Create(mapObject);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<MapObjectDto> Update([FromBody] MapObjectDto mapObject)
+        {
+            var result = _mapObjectService.Update(mapObject);
+            return CreateResponse(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var result = _mapObjectService.Delete(id);
+            return CreateResponse(result);
+        }
+    }
+}
--- src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
@@ -1,3 +1,5 @@
+DELETE FROM tours."Equipment";
+DELETE FROM tours."MapObjects";
 DELETE FROM tours."Checkpoints";
 DELETE FROM tours."TourEquipment";
 DELETE FROM tours."Equipment";;
