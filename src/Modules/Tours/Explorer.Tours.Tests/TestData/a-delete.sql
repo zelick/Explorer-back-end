ef7b84e8c6db9120b2291dafce7f78424386a3d6
Anja Lovric <anja.lovric@gmail.com>
2023-10-24 17:46:43 +0200

    fix: test data script fixed

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
@@ -2,5 +2,5 @@ DELETE FROM tours."Checkpoints";
 DELETE FROM tours."TourEquipment";
 DELETE FROM tours."Equipment";;
 DELETE FROM tours."TourPreference";
-DELETE FROM tours."Tours";
 DELETE FROM tours."ReportedIssues";
+DELETE FROM tours."Tours";
