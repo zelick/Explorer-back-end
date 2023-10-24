f8009309c6133a7f9207cb0d6d36983b2947672b
Stefaan01 <116906194+Stefaan01@users.noreply.github.com>
2023-10-24 22:32:12 +0200

    fix: database

--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/d-grades.sql
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/d-grades.sql
@@ -1,12 +1,12 @@
 INSERT INTO stakeholders."ApplicationGrades"(
 	"Id", "Rating", "Comment", "Created", "UserId")
-	VALUES (-1, 1, 'Bad', to_timestamp('2023-10-24T12:00:00Z', 'YYYY-MM-DD"T"HH24:MI:SS"Z"'), -1);
+	VALUES (-1, 1, 'Bad', to_timestamp('2023-10-24T12:00:00Z', 'YYYY-MM-DD"T"HH24:MI:SS"Z"'), '-1');
 INSERT INTO stakeholders."ApplicationGrades"(
 	"Id", "Rating", "Comment", "Created", "UserId")
-	VALUES (-2, 2, 'Nothing special', to_timestamp('2023-10-22T12:00:00Z', 'YYYY-MM-DD"T"HH24:MI:SS"Z"'), -11);
+	VALUES (-2, 2, 'Nothing special', to_timestamp('2023-10-22T12:00:00Z', 'YYYY-MM-DD"T"HH24:MI:SS"Z"'), '-11');
 INSERT INTO stakeholders."ApplicationGrades"(
 	"Id", "Rating", "Comment", "Created", "UserId")
-	VALUES (-3, 3, 'Good', to_timestamp('2023-10-20T12:00:00Z', 'YYYY-MM-DD"T"HH24:MI:SS"Z"'), -12);
+	VALUES (-3, 3, 'Good', to_timestamp('2023-10-20T12:00:00Z', 'YYYY-MM-DD"T"HH24:MI:SS"Z"'), '-12');
 INSERT INTO stakeholders."ApplicationGrades"(
 	"Id", "Rating", "Comment", "Created", "UserId")
-	VALUES (-4, 4, '', to_timestamp('2023-10-18T12:00:00Z', 'YYYY-MM-DD"T"HH24:MI:SS"Z"'), -13);
+	VALUES (-4, 4, '', to_timestamp('2023-10-18T12:00:00Z', 'YYYY-MM-DD"T"HH24:MI:SS"Z"'), '-13');
