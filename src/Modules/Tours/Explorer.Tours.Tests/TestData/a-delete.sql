1d8d2d0f5d78386289547809ce43ef242093d380
Milica Vujić <milicavujic2001@gmail.com>
2023-10-25 18:07:53 +0200

    fix: Test for tour fixed

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
@@ -2,7 +2,7 @@ DELETE FROM tours."Checkpoints";
 DELETE FROM tours."TourEquipment";
 DELETE FROM tours."Equipment";;
 DELETE FROM tours."TourPreference";
+DELETE FROM tours."TourRating";
 DELETE FROM tours."ReportedIssues";
 DELETE FROM tours."Tours";
 DELETE FROM tours."MapObjects";
-DELETE FROM tours."TourRating";
