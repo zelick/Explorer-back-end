1419e54cf1eb74ec7082d9837d56ca732202c936
Milica Vujić <milicavujic2001@gmail.com>
2023-11-07 17:31:57 +0100

    feat: TourExecution and CheckpointCompletition models created

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/CheckoutCompletition.cs
@@ -0,0 +1,17 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class CheckoutCompletition: Entity
+    {
+        public long CheckpointId { get; init; }
+        public DateTime CompletitionTime { get; init; }
+
+        public CheckoutCompletition(long checkpointId)
+        {
+            CheckpointId = checkpointId;
+            CompletitionTime = DateTime.Now;
+        }
+
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourExecution.cs
@@ -0,0 +1,48 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class TourExecution:Entity
+    {
+        public long TouristId { get; init; }
+        public long TourId { get; init; }
+        public Tour Tour { get; init; }
+        public DateTime Start {  get; init; }
+        public  DateTime LastActivity { get; init; }
+        public ExecutionStatus ExecutionStatus { get; init; }
+
+        public TourExecution(long touristId,long tourId) 
+        {
+            if(touristId < 0)
+            {
+                throw new ArgumentException("Invalid tourist.");
+            }
+            if (tourId < 0)
+            {
+                throw new ArgumentException("Invalid tour.");
+            }
+
+            TouristId = touristId;
+            TourId = tourId;
+            Start= DateTime.Now;
+            LastActivity = DateTime.Now;
+            ExecutionStatus = ExecutionStatus.InProgress;
+
+        }
+
+        public void RegisterActivity(TouristPosition touristPosition)
+        {
+            
+        }
+
+    }
+
+    public enum ExecutionStatus
+    {
+        Completed,
+        Abandoned,
+        InProgress
+    }
+
+
+}
