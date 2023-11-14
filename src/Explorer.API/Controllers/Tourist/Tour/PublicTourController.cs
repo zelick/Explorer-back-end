a9a02644e215c96c8c0aa8b3704bc3df98a2aa64
zelick <116888497+zelick@users.noreply.github.com>
2023-11-14 23:22:01 +0100

    test: PublicTourCommandTest

--- src/Explorer.API/Controllers/Tourist/Tour/PublicTourController.cs
+++ src/Explorer.API/Controllers/Tourist/Tour/PublicTourController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Explorer.API.Controllers.Tourist.Tour
 {
-  //  [Authorize(Policy = "touristPolicy")]
+    [Authorize(Policy = "touristPolicy")]
     [Route("api/tourist/publicTours")]
     public class PublicTourController : BaseApiController
     {
---
+++ src/Modules/Tours/Explorer.Tours.Tests/Integration/Tourism/PublicTourCommandTests.cs
@@ -0,0 +1,46 @@
+﻿using Explorer.API.Controllers.Tourist.Tour;
+using Explorer.API.Controllers.Tourist.Tourism;
+using Explorer.Stakeholders.Infrastructure.Database;
+using Explorer.Tours.API.Public.Administration;
+using Explorer.Tours.Infrastructure.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Tests.Integration.Tourism
+{
+    [Collection("Sequential")]
+    public class PublicTourCommandTests : BaseToursIntegrationTest
+    {
+
+        public PublicTourCommandTests(ToursTestFactory factory) : base(factory) { }
+
+        [Fact]
+        public void getAllPublicTours_Succeed()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var dbContext = scope.ServiceProvider.GetRequiredService<ToursContext>();
+
+            // Act
+            var result = (ObjectResult)controller.GetPublicTours().Result;
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldBe(200);
+        }
+
+        private static PublicTourController CreateController(IServiceScope scope)
+        {
+            return new PublicTourController(scope.ServiceProvider.GetRequiredService<ITourService>())
+            {
+                ControllerContext = BuildContext("-1")
+            };
+        }
+
+    }
+}
