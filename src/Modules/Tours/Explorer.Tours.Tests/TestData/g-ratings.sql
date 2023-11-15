987c2df7a1a6edd3f0cb6cdbbc2b293b2d61bb82
Anja Lovric <anja.lovric@gmail.com>
2023-11-15 13:15:35 +0100

    fix: tests data for tour rating fixed

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/g-ratings.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/g-ratings.sql
@@ -1,3 +1,3 @@
 INSERT INTO tours."TourRating"(
 	"Id", "Rating", "Comment", "TouristId", "TourId", "TourDate", "CreationDate", "ImageNames")
-VALUES (-1, 5, 'Ekstra je tura, jako mi se svidelo', -22, -4, '13/11/2023 14:00:00', '13/11/2023 14:00:00', ARRAY ['slika']);
+VALUES (-1, 5, 'Ekstra je tura, jako mi se svidelo', -22, -4, '12-11-2023 14:00:00', '12-11-2023 14:00:00', ARRAY ['slika']);
