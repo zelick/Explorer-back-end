9761230a63b183853edda756a3c40f662b22a57c
petrovicmilica <116906072+petrovicmilica@users.noreply.github.com>
2023-11-29 20:44:53 +0100

    test: Create tour bundle

---
+++ src/Explorer.API/Controllers/Author/Administration/TourBundleController.cs
@@ -0,0 +1,25 @@
+﻿using Explorer.API.Services;
+using Explorer.Blog.Core.Domain.BlogPosts;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Stakeholders.Infrastructure.Authentication;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Explorer.Tours.Core.UseCases.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Author.Administration
+{
+	[Authorize(Policy = "authorPolicy")]
+	[Route("api/administration/tour-bundle")]
+	public class TourBundleController : BaseApiController
+	{
+		public TourBundleController() { }
+
+		[HttpPost]
+		public ActionResult<TourBundleDto> Create([FromBody] TourBundleDto tourBundle)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/Modules/Tours/Explorer.Tours.Tests/Integration/Administration/TourBundleCommandTests.cs
@@ -0,0 +1,43 @@
+﻿using Explorer.API.Controllers.Author.Administration;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Explorer.Tours.Infrastructure.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Tests.Integration.Administration
+{
+	[Collection("Sequential")]
+	public class TourBundleCommandTests : BaseToursIntegrationTest
+	{
+		public TourBundleCommandTests(ToursTestFactory factory) : base(factory) { }
+
+		[Fact]
+		public void Creates()
+		{
+			// Arrange
+			var controller = CreateController();
+			var newEntity = new TourBundleDto();
+
+			// Act
+			var result = ((ObjectResult)controller.Create(newEntity).Result)?.Value as TourBundleDto;
+
+			// Assert - Response
+			result.ShouldNotBeNull();
+		}
+
+		private static TourBundleController CreateController()
+		{
+			return new TourBundleController()
+			{
+				ControllerContext = BuildContext("-12")
+			};
+		}
+	}
+}
