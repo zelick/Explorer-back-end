9ad19662f5984fb41a2f9db05ee72c14d6532a22
Uslje <147374020+uslje26@users.noreply.github.com>
2023-11-30 19:33:09 +0100

    test: coupon command and query tests

---
+++ src/Modules/Payments/Explorer.Payments.Tests/Integration/CouponCommandTests.cs
@@ -0,0 +1,118 @@
+﻿using Explorer.API.Controllers.Author.Administration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace Explorer.Payments.Tests.Integration
+{
+    [Collection("Sequential")]
+    public class CouponCommandTests : BasePaymentsIntegrationTest
+    {
+        public CouponCommandTests(PaymentsTestFactory factory) : base(factory) { }
+
+        [Fact]
+        public void Creates()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var coupon = new Object();
+
+            // Act
+            var result = ((ObjectResult)controller.Create(coupon).Result).Value as Object;
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.ShouldBe(200);
+        }
+
+        [Fact]
+        public void Create_fails_invalid_data()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var coupon = new Object();
+
+            // Act
+            var result = ((ObjectResult)controller.Create(coupon).Result).Value as Object;
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.ShouldBe(400);
+        }
+
+        [Fact]
+        public void Updates()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var updatedCoupon = new Object();
+
+            // Act
+            var result = ((ObjectResult)controller.Update(updatedCoupon).Result).Value as Object;
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.ShouldBe(200);
+        }
+
+        [Fact]
+        public void Update_fails_invalid_id()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var updatedCoupon = new Object();
+
+            // Act
+            var result = ((ObjectResult)controller.Update(updatedCoupon).Result).Value as Object;
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.ShouldBe(400);
+        }
+
+
+        [Fact]
+        public void Deletes()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var deletedCouponId = -1;
+
+            // Act
+            var result = ((ObjectResult)controller.Delete(deletedCouponId)).Value;
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.ShouldBe(200);
+        }
+
+        [Fact]
+        public void Delete_fails_invalid_id()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var deletedCouponId = -1;
+
+            // Act
+            var result = ((ObjectResult)controller.Delete(deletedCouponId)).Value;
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.ShouldBe(404);
+        }
+
+        private static CouponController CreateController(IServiceScope scope)
+        {
+            return new CouponController(/*scope.ServiceProvider.GetRequiredService<ICouponService>()*/)
+            {
+                ControllerContext = BuildContext("-1")
+            };
+        }
+    }
+}
---
+++ src/Modules/Payments/Explorer.Payments.Tests/Integration/CouponQueryTests.cs
@@ -0,0 +1,38 @@
+﻿using Explorer.API.Controllers.Author.Administration;
+using Explorer.Blog.API.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace Explorer.Payments.Tests.Integration
+{
+    [Collection("Sequential")]
+    public class CouponQueryTests : BasePaymentsIntegrationTest
+    {
+        public CouponQueryTests(PaymentsTestFactory factory) : base(factory) { }
+
+        [Fact]
+        public void Retrieves_all()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+
+            // Act
+            var result = (ObjectResult)controller.GetAll(0, 0).Result;
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldBe(200);
+        }
+
+
+        private static CouponController CreateController(IServiceScope scope)
+        {
+            return new CouponController(/*scope.ServiceProvider.GetRequiredService<ICouponService>()*/)
+            {
+                ControllerContext = BuildContext("-1")
+            };
+        }
+    }
+}
