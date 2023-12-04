cf18dc9842cca0ca7e67d3f353897fd6ec2e4f03
Lara <larapetkovic021@gmail.com>
2023-12-04 17:25:05 +0100

    fix: PrivateTour test data fixed

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/e-privateTours.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/e-privateTours.sql
@@ -1,9 +1,9 @@
 ﻿INSERT INTO tours."PrivateTours"(
-	"Id", "ToursitId", "Name", "Checkpoints", "Execution")
+	"Id", "TouristId", "Name", "Checkpoints", "Execution")
 	VALUES (-1, -3, 'moja tura 1', '[]', null);
 INSERT INTO tours."PrivateTours"(
-	"Id", "ToursitId", "Name", "Checkpoints", "Execution")
+	"Id", "TouristId", "Name", "Checkpoints", "Execution")
 	VALUES (-2, -3, 'moja tura 2', '[]', null);
 INSERT INTO tours."PrivateTours"(
-	"Id", "ToursitId", "Name", "Checkpoints", "Execution")
+	"Id", "TouristId", "Name", "Checkpoints", "Execution")
 	VALUES (-3, -5, 'moja tura 3', '[]', null);
