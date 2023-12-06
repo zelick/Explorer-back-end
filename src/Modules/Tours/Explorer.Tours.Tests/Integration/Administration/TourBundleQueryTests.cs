ac7d7517251bae92450aa66a3f7fc005f96da883
Anastasija <anastasijamarsenic@gmail.com>
2023-12-06 18:11:12 +0100

    test: update tour bundle tests

--- src/Modules/Tours/Explorer.Tours.Tests/Integration/Administration/TourBundleQueryTests.cs
+++ src/Modules/Tours/Explorer.Tours.Tests/Integration/Administration/TourBundleQueryTests.cs
@@ -5,72 +5,79 @@ using Explorer.Tours.API.Public.Administration;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Explorer.Tours.Tests.Integration.Administration
+namespace Explorer.Tours.Tests.Integration.Administration;
+
+[Collection("Sequential")]
+public class TourBundleQueryTests : BaseToursIntegrationTest
 {
-	[Collection("Sequential")]
-	public class TourBundleQueryTests : BaseToursIntegrationTest
-	{
-		public TourBundleQueryTests(ToursTestFactory factory) : base(factory) { }
+    public TourBundleQueryTests(ToursTestFactory factory) : base(factory) { }
 
-		[Fact]
-		public void Retrieves_all()
-		{
-			// Arrange
-			using var scope = Factory.Services.CreateScope();
-			var controller = CreateController(scope);
+    [Fact]
+    public void Retrieves_all_published()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var controller = CreateController(scope);
+        // Act
+        var result = ((ObjectResult)controller.GetAllPublished(0, 0).Result)?.Value as PagedResult<TourBundleDto>;
+        // Assert
+        result.ShouldNotBeNull();
+        result.Results.Count.ShouldBe(1);
+    }
 
-			// Act
-			var result = ((ObjectResult)controller.GetAll(0, 0).Result)?.Value as PagedResult<TourBundleDto>;
+    [Fact]
+    public void Retrieves_all()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var controller = CreateController(scope);
 
-			// Assert
-			result.ShouldNotBeNull();
-			result.Results.Count.ShouldBe(3);
-			result.TotalCount.ShouldBe(3);
-		}
+        // Act
+        var result = ((ObjectResult)controller.GetAll(0, 0).Result)?.Value as PagedResult<TourBundleDto>;
 
-		[Fact]
-		public void Retrieves_all_by_author()
-		{
-			// Arrange
-			using var scope = Factory.Services.CreateScope();
-			var controller = CreateController(scope);
+        // Assert
+        result.ShouldNotBeNull();
+        result.Results.Count.ShouldBe(3);
+        result.TotalCount.ShouldBe(3);
+    }
 
-			// Act
-			var result = ((ObjectResult)controller.GetAllByAuthor(0, 0).Result)?.Value as List<TourBundleDto>;
+    [Fact]
+    public void Retrieves_all_by_author()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var controller = CreateController(scope);
 
-			// Assert
-			result.ShouldNotBeNull();
-			result.Count.ShouldBe(3);
-		}
-		[Fact]
-		public void Retrieves_one()
-		{
-			// Arrange
-			using var scope = Factory.Services.CreateScope();
-			var controller = CreateController(scope);
+        // Act
+        var result = ((ObjectResult)controller.GetAllByAuthor(0, 0).Result)?.Value as List<TourBundleDto>;
 
-			// Act
-			var result = ((ObjectResult)controller.GetById(-2).Result)?.Value as TourBundleDto;
+        // Assert
+        result.ShouldNotBeNull();
+        result.Count.ShouldBe(3);
+    }
+    [Fact]
+    public void Retrieves_one()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var controller = CreateController(scope);
 
-			// Assert
-			result.ShouldNotBeNull();
-			result.Name.ShouldBe("Paket tura 2");
-			result.AuthorId.ShouldBe(-11);
-		}
+        // Act
+        var result = ((ObjectResult)controller.GetById(-2).Result)?.Value as TourBundleDto;
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.Name.ShouldBe("Paket tura 2");
+        result.AuthorId.ShouldBe(-11);
+    }
 
 
-		private static TourBundleController CreateController(IServiceScope scope)
-		{
-			return new TourBundleController(scope.ServiceProvider.GetRequiredService<ITourBundleService>())
-			{
-				ControllerContext = BuildContext("-11")
-			};
-		}
-	}
+    private static TourBundleController CreateController(IServiceScope scope)
+    {
+        return new TourBundleController(scope.ServiceProvider.GetRequiredService<ITourBundleService>())
+        {
+            ControllerContext = BuildContext("-11")
+        };
+    }
 }
