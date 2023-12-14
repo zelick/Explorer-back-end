af27580dfebd65c1a64fa684c8ae6f62c6560a5c
Lara <larapetkovic021@gmail.com>
2023-12-14 18:56:43 +0100

    feat: Coupon function finished

--- src/Modules/Payments/Explorer.Payments.Core/Domain/RepositoryInterfaces/IShoppingCartRepository.cs
+++ src/Modules/Payments/Explorer.Payments.Core/Domain/RepositoryInterfaces/IShoppingCartRepository.cs
@@ -3,6 +3,7 @@
 public interface IShoppingCartRepository
 {
     ShoppingCart Get(long id);
+    Coupon GetByCouponText(string couponText);
     public ShoppingCart GetByUser(long userId);
     ShoppingCart Create(ShoppingCart shoppingCart);
     ShoppingCart Update(ShoppingCart shoppingCart);
