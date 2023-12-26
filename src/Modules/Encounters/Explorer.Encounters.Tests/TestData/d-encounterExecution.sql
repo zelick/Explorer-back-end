c560cd00ecc37623a5a4192e5188b72690bb7cc2
jelly0107 <jelisaveta.letic@gmail.com>
2023-12-26 20:19:56 +0100

    test: adding tests for CompleteEncounter

--- src/Modules/Encounters/Explorer.Encounters.Tests/TestData/d-encounterExecution.sql
+++ src/Modules/Encounters/Explorer.Encounters.Tests/TestData/d-encounterExecution.sql
@@ -5,3 +5,7 @@ INSERT INTO encounters."EncounterExecution"(
 INSERT INTO encounters."EncounterExecution"(
 	"Id", "EncounterId", "TouristId", "Status", "StartTime", "EndTime")
 	VALUES (-2, -5, -23, 0, '2023-11-11 02:37:10.351502+01',  '-infinity');
+
+INSERT INTO encounters."EncounterExecution"(
+	"Id", "EncounterId", "TouristId", "Status", "StartTime", "EndTime")
+	VALUES (-3, -1, -23, 2, '2023-11-11 02:37:10.351502+01',  '-infinity');
